15b95d5d271c84ab3e1733512c09e11cfcf2d18a
testpubg870 <90134113+testpubg870@users.noreply.github.com>
2021-09-06 19:35:00 +0530

    Update backup.py

--- tortoolkit/functions/backup.py
+++ tortoolkit/functions/backup.py
@@ -1,1 +1,23 @@
+import asyncio
+import logging
 
+
+logging.getLogger("telethon").setLevel(logging.WARNING)
+torlog = logging.getLogger(__name__)
+
+
+async def backup_file(e):
+  if not e.is_reply:
+        await e.reply("Reply to Drive Upload Successfull Message.")
+  elif not check_for_index(e):
+        await e.reply("Index URL Button Not Present.")
+  else:
+    
+    
+    
+def check_for_index(e):
+  if (await e.get_reply_message()).buttons is None:
+        return False
+  for i in (await e.get_reply_message()).buttons:
+        
+  
