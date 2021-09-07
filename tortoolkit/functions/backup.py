91fda52e58108e2b3c88349599b011bbcfeafa3e
testpubg870 <90134113+testpubg870@users.noreply.github.com>
2021-09-07 10:30:40 +0530

    Update backup.py

--- tortoolkit/functions/backup.py
+++ tortoolkit/functions/backup.py
@@ -89,3 +89,15 @@ def calculate_size(path):
     else:
         return 0
             
+def get_size_fl(start_path="."):
+    total_size = 0
+    for dirpath, _, filenames in os.walk(start_path):
+        for f in filenames:
+            fp = os.path.join(dirpath, f)
+            # skip if it is symbolic link
+            if not os.path.islink(fp):
+                total_size += os.path.getsize(fp)
+
+    return total_size
+
+
