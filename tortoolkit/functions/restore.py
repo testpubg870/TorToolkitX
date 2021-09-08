637bf2db9f602edc926a9ae26d7ecf1cf5fd8f54
testpubg870 <90134113+testpubg870@users.noreply.github.com>
2021-09-08 16:18:59 +0530

    Update restore.py

--- tortoolkit/functions/restore.py
+++ tortoolkit/functions/restore.py
@@ -1,1 +1,22 @@
+import logging
 
+logging.getLogger("telethon").setLevel(logging.WARNING)
+torlog = logging.getLogger(__name__)
+
+async def restore_single_file(e):
+    if not e.is_reply:
+        await e.reply("Reply to Backup Channel Message.")
+    elif not await check_for_media(e):
+        await e.reply("No Media In Replied Message.")
+    else:
+        e.reply((await e.get_reply_message()).media)
+          
+          
+          
+          
+async def check_for_media(e):
+    if (await e.get_reply_message()).media is None:
+        return False
+    else:
+        return True
+    return False
