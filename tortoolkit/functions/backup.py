ccade1958c897559b27302b7a775addb179dd010
testpubg870 <90134113+testpubg870@users.noreply.github.com>
2021-09-08 11:25:29 +0530

    Update backup.py

--- tortoolkit/functions/backup.py
+++ tortoolkit/functions/backup.py
@@ -53,7 +53,7 @@ async def backup_file(e):
                     url=url
                    #task=ul_task,
                 )
-                await e.reply("Backup Complete.)
+                await e.reply("Backup Complete.")
             except:
                 rdict = dict()
                 torlog.exception("Exception in Direct links.")
