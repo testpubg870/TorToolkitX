ff192a49b06bca1e1cbe8d89c1262d26cf45f8c2
testpubg870 <90134113+testpubg870@users.noreply.github.com>
2021-09-09 12:52:23 +0530

    Update restore.py

--- tortoolkit/functions/restore.py
+++ tortoolkit/functions/restore.py
@@ -20,8 +20,8 @@ async def restore_single_file(e):
         start_time = time.time()
         tout = get_val("EDIT_SLEEP_SECS")
         path = (await e.get_reply_message()).raw_text
-        file_id = (await e.get_reply_message()).file.id
-        send_message = await reply_message.client.pyro.download_media(file_id,
+        reply_message = (await e.get_reply_message()).media
+        send_message = await rmsg.client.pyro.download_media(reply_message,
                                                                       file_name=path,
                                                                       progress=progress_for_pyrogram,
                                                                       progress_args=(os.path.basename(path),
