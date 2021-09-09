f2ccddaab57afd9e66b4030dffa223d4e29267f3
testpubg870 <90134113+testpubg870@users.noreply.github.com>
2021-09-09 18:38:08 +0530

    Update restore_progress.py

--- tortoolkit/functions/restore_progress.py
+++ tortoolkit/functions/restore_progress.py
@@ -52,7 +52,7 @@ async def progress_for_pyrogram(
         elapsed_time = human_readable_timedelta(elapsed_time)
         estimated_total_time = human_readable_timedelta(estimated_total_time)
 
-        progress = "[{0}{1}] \nP: {2}%\n".format(
+        progress = "[{0}{1}] \n**Percentage:** {2}%\n".format(
             "".join(
                 [get_val("COMPLETED_STR") for _ in range(math.floor(percentage / 10))]
             ),
@@ -67,7 +67,7 @@ async def progress_for_pyrogram(
 
         tmp = (
             progress
-            + "{0} of {1}\nSpeed: {2}/s\nETA: {3}\nUsing engine: Pyrogram".format(
+            + "**Downloaded:** {0} of {1}\n**Speed:** {2}/s\n**ETA:** {3}\n**Using engine:** Pyrogram".format(
                 human_readable_bytes(current),
                 human_readable_bytes(total),
                 human_readable_bytes(speed),
