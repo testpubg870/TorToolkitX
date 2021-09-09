69cad173913c122f85c218836fd6a86762c78f05
testpubg870 <90134113+testpubg870@users.noreply.github.com>
2021-09-09 12:19:55 +0530

    Update restore.py

--- tortoolkit/functions/restore.py
+++ tortoolkit/functions/restore.py
@@ -1,7 +1,13 @@
 import logging
+import os
+import time
+
 
 logging.getLogger("telethon").setLevel(logging.WARNING)
 torlog = logging.getLogger(__name__)
+
+from .restore_progress import progress_for_pyrogram
+from ..core.getVars import get_val
 
 async def restore_single_file(e):
     if not e.is_reply:
@@ -10,8 +16,21 @@ async def restore_single_file(e):
         await e.reply("No Media In Replied Message.")
     else:
         rmsg = await e.reply("Processing Media.")
-        print()
-        await e.reply((await e.get_reply_message()).media)
+        start_time = time.time()
+        tout = get_val("EDIT_SLEEP_SECS")
+        path = (await e.get_reply_message()).raw_text
+        reply_message = await e.get_reply_message()
+        send_message = await reply_message.client.pyro.download(file_name=path,
+                                                 progress=progress_for_pyrogram,
+                                                 progress_args=(os.path.basename(path),
+                                                               rmsg,
+                                                               start_time,
+                                                               tout,
+                                                               e.client.pyro
+                                                               ))
+        
+        
+        clear_stuff(path)
           
           
           
@@ -21,3 +40,14 @@ async def check_for_media(e):
         return False
     else:
         return True
+    
+async def clear_stuff(path):
+    try:
+        if os.path.isdir(path):
+            shutil.rmtree(path)
+        else:
+            os.remove(path)
+        torlog.warning("File(s) Deleted.")
+    except:
+        torlog.warning("Failed to Delete File(s).")
+        pass
