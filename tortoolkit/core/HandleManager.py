dc11df974ae11a07232b4f467fdd9be074b55f1c
testpubg870 <90134113+testpubg870@users.noreply.github.com>
2021-09-10 09:22:42 +0530

    Update HandleManager.py

--- tortoolkit/core/HandleManager.py
+++ tortoolkit/core/HandleManager.py
@@ -659,7 +659,7 @@ async def set_password_zip(message):
 
 
 async def start_handler(event):
-    msg = "Hello This is TorToolkitX running on heroku an instance of <a href='https://github.com/XcodersHub/TorToolkitX'>This Repo</a>. Try the repo for yourself and dont forget to put a STAR and fork."
+    msg = "I am Alive."
     await event.reply(msg, parse_mode="html")
 
 
